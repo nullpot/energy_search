171877dbe05d043d10b04aa8f4668b0c6cb1dfe7
itagaki <itagaki.shintaro@gmail.com>
2015-01-17 16:01:10 +0900

    Add twitter.js

--- js/twitter.js
+++ js/twitter.js
@@ -1,7 +1,13 @@
+var name = drinks[Number(selectedObjects[0])].name;
+
 $.ajax({
-    url: "http://157.7.204.121/twitter_search.php?word=レッドブル",
+    url: "http://157.7.204.121/twitter_search.php?word=" + name,
 }).done(function(data){
-    console.log(data);
+	data.forEach(function(tweet, index){
+		$(".drink-tweet").append("<div class='tweet_text'>");
+		$(".drink-tweet").append(tweet.text);
+		$(".drink-tweet").append("</div>");
+	});
 }).fail(function(error){
     console.log(error);
 });
