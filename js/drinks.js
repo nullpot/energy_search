99eb9dcc2342baaced40ea1be01e23f7299bc2b0
itagaki <itagaki.shintaro@gmail.com>
2015-01-17 13:40:34 +0900

    Add チオビタドリンクのデータ

--- js/drinks.js
+++ js/drinks.js
@@ -111,23 +111,24 @@ var drinks = [{
 }, {
     name: "チオビタドリンク",
     image: ""
-    energy: 0,
+    energy: 70,
     protein: 0,
     fat: 0,
     natrium: 0,
     carbohydrate: 0,
     vitamin_b: 0,
-    vitamin_b2: 0,
-    vitamin_b6: 0,
+    vitamin_b2: 5,
+    vitamin_b6: 5,
     vitamin_b12: 0,
     vitamin_c: 0,
     niacin: 0,
-    inositol: 0,
-    caffeine: 0,
+    inositol: 50,
+    caffeine: 50,
     arginine: 0,
     pantothenic_acid: 0,
     valine: 0,
     isoleucine: 0,
     leucine: 0,
-    d_ribose: 0
+    d_ribose: 0,
+    taurine: 1000
 }];
